2db75c44d1de726bcf9dde6394c9e1d0909a89d5
JayKearney <indi.mh@gmail.com>
2021-10-11 08:57:41 +0000

    Change class to id and inner.html is loggin now. Only logs well done so change switch function to else if and console.log to troubleshoot

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,10 @@ function compareBudgetwithExpenses(budget,expenses){
     if (budget > expenses) {
         resultAlert.innerHTML = "Well done";
 
-    } else switch (budget) {
-    case expenses:
-        resultAlert.innerHTML = "Be careful";
-        break;
-    default:
-        resultAlert.innerHTML = "Wrong";
-        break;
+    } else if(budget == expenses){
+        resultAlert.innerHTML = "Careful";
+    }else{
+        resultAlert.innerHTML = "Bad";
     }
 }
 
@@ -36,17 +33,30 @@ submitButton.addEventListener("click",function(e){
 
     // What will happen when the user clicks the button
     
-    let rentmortgageValue = rentmortgageInput.value;
-    let electricityValue = electricityInput.value;
-    let gasValue = gasInput.value;
-    let internetphoneValue = internetphoneInput.value;
-    let groceriesValue = groceriesInput.value;
-    let transportationValue = transportationInput.value;
-    let subscriptionsValue = subscriptionsInput.value;
-    let entertainmentValue = entertainmentInput.value;
-    let personalValue = personalInput.value;
     let budgetValue = budgetInput.value;
+    console.log(budgetValue);
+    let rentmortgageValue = rentmortgageInput.value;
+    console.log(rentmortgageValue);
+    let electricityValue = electricityInput.value;
+    console.log(electricityValue);
+    let gasValue = gasInput.value;
+    console.log(gasValue);
+    let internetphoneValue = internetphoneInput.value;
+    console.log(internetphoneValue);
+    let groceriesValue = groceriesInput.value;
+    console.log(groceriesValue);
+    let transportationValue = transportationInput.value;
+    console.log(transportationValue);
+    let subscriptionsValue = subscriptionsInput.value;
+    console.log(subscriptionsValue);
+    let entertainmentValue = entertainmentInput.value;
+    console.log(entertainmentValue);
+    let personalValue = personalInput.value;
+    console.log(personalValue);
+    
 
+    
+    
 
     // What will happen if a field is empty
 
