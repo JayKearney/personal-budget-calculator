73197f474e218ee564f491aff3743a5dcf68172d
JayKearney <indi.mh@gmail.com>
2021-10-17 10:24:26 +0000

    replace huge switch statement by putting all values in an object and looping over to check for empty values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,34 @@
-
 // References to budget calculator fields
 
-const budgetInput = document.getElementById("budget-input");
-const rentmortgageInput = document.getElementById("rent-mortgage-input");
-const electricityInput = document.getElementById("electricity-input");
-const gasInput = document.getElementById("gas-input");
-const internetphoneInput = document.getElementById("int-phone-input");
-const groceriesInput = document.getElementById("groceries-input");
-const transportationInput = document.getElementById("transp-input");
-const subscriptionsInput = document.getElementById("subs-input");
-const entertainmentInput = document.getElementById("ent-input");
-const personalInput = document.getElementById("personal-input");
-const submitButton = document.getElementById("submit-button");
-const expensestotalInput = document.getElementById("expenses-total");
-var resultAlert = document.getElementById("result-alert");
+const budgetInput = document.getElementById('budget-input');
+const rentmortgageInput = document.getElementById('rent-mortgage-input');
+const electricityInput = document.getElementById('electricity-input');
+const gasInput = document.getElementById('gas-input');
+const internetphoneInput = document.getElementById('int-phone-input');
+const groceriesInput = document.getElementById('groceries-input');
+const transportationInput = document.getElementById('transp-input');
+const subscriptionsInput = document.getElementById('subs-input');
+const entertainmentInput = document.getElementById('ent-input');
+const personalInput = document.getElementById('personal-input');
+const submitButton = document.getElementById('submit-button');
+const expensestotalInput = document.getElementById('expenses-total');
+var resultAlert = document.getElementById('result-alert');
 
-function compareBudgetwithExpenses(budget,expenses){
-    console.log("Budget: ", budget);
-    console.log("Expenses: ", expenses);
+function compareBudgetwithExpenses(budget, expenses) {
+    console.log('Budget: ', budget);
+    console.log('Expenses: ', expenses);
     if (budget > expenses) {
-        resultAlert.innerHTML = "Well done";
-
-    } else if(budget == expenses){
-        resultAlert.innerHTML = "Careful";
-    }else{
-        resultAlert.innerHTML = "Bad";
+        resultAlert.innerHTML = 'Well done';
+    } else if (budget == expenses) {
+        resultAlert.innerHTML = 'Careful';
+    } else {
+        resultAlert.innerHTML = 'Bad';
     }
 }
 
 // Display the values
 
-submitButton.addEventListener("click",function(e){
-
+submitButton.addEventListener('click', function (e) {
     // What will happen when the user clicks the button
 
     let budgetValue = budgetInput.value;
@@ -55,104 +52,54 @@ submitButton.addEventListener("click",function(e){
     let personalValue = personalInput.value;
     console.log(personalValue);
 
+    //All required values are stored together with a label
 
+    const requiredValues = [
+        { label: 'Rent/Mortgage', value: rentmortgageValue },
+        { label: 'Electricity', value: electricityValue },
+        { label: 'Gas', value: gasValue },
+        { label: 'Internet/Phone', value: internetphoneValue },
+        { label: 'Groceries', value: groceriesValue },
+        { label: 'Transportation', value: transportationValue },
+        { label: 'Subscription', value: subscriptionsValue },
+        { label: 'Entertainment', value: entertainmentValue },
+        { label: 'Personal', value: personalValue },
+        { label: 'Budget', value: budgetValue },
+    ];
 
+    // Go through each object and check whether a value has been inputted.
+    // If a missing value is found, an alert with the correct label is displayed
 
-
-    switch (rentmortgageValue) {
-    case "":
-        alert("Insert a number in the Rent/Mortgage field");
-        break;
-    default:
-        switch (electricityValue) {
-        case "":
-            alert("Insert a number in the Electricity field");
-            break;
-        default:
-            switch (gasValue) {
-            case "":
-                alert("Insert a number in the Gas field");
-                break;
-            default:
-                switch (internetphoneValue) {
-                case "":
-                    alert("Insert a number in the internet/phone field");
-                    break;
-                default:
-                    switch (groceriesValue) {
-                    case "":
-                        alert("Insert a number in the Groceries field");
-                        break;
-                    default:
-                        switch (transportationValue) {
-                        case "":
-                            alert("Insert a number in the Transportation field");
-                            break;
-                        default:
-                            switch (subscriptionsValue) {
-                            case "":
-                                alert("Insert a number in the subscription field");
-                                break;
-                            default:
-                                switch (entertainmentValue) {
-                                case "":
-                                    alert("Insert a number in the entertainment field");
-                                    break;
-                                default:
-                                    switch (personalValue) {
-                                    case "":
-                                        alert("Insert a number in the Personal field");
-                                        break;
-                                    default:
-                                        switch (budgetValue) {
-                                        case "":
-                                            alert("Insert a number in the Budget field");
-                                            break;
-                                        default:
-
-
-                                            // Calculation when all values have been entered
-
-                                            var budget = budgetValue;
-
-                                            var expenses = [parseInt(rentmortgageValue) , parseInt(electricityValue) , parseInt(gasValue) , parseInt(internetphoneValue) , parseInt(groceriesValue) , parseInt(transportationValue) , parseInt(subscriptionsValue) , parseInt(entertainmentValue) , parseInt(personalValue)];
-                                            var sum = 0;
-                                            for (let i = 0; i < expenses.length; i++) {
-                                                sum += expenses[i];
-                                            }
-                                            
-                                            balance = parseInt(budgetValue) - sum;
-
-                                            expensestotalInput.value = balance;
-
-                                            compareBudgetwithExpenses(budget,sum);
-                                            break;
-                                        }
-
-                                        break;
-                                    }
-
-                                    break;
-                                }
-
-                                break;
-                            }
-
-                            break;
-                        }
-
-                        break;
-                    }
-
-                    break;
-                }
-
-                break;
-            }
-
-            break;
+    for (let i = 0; i < requiredValues.length; i++) {
+        if (requiredValues[i].value === '') {
+            alert(`Insert a number in the ${requiredValues[i].label} field`);
+            return;
         }
-
-        break;
     }
+
+    // Calculation when all values have been entered
+
+    let budget = budgetValue;
+
+    const expenses = [
+        parseInt(rentmortgageValue),
+        parseInt(electricityValue),
+        parseInt(gasValue),
+        parseInt(internetphoneValue),
+        parseInt(groceriesValue),
+        parseInt(transportationValue),
+        parseInt(subscriptionsValue),
+        parseInt(entertainmentValue),
+        parseInt(personalValue),
+    ];
+
+    let sum = 0;
+    for (let i = 0; i < expenses.length; i++) {
+        sum += expenses[i];
+    }
+
+    balance = parseInt(budgetValue) - sum;
+    expensestotalInput.value = balance;
+
+    compareBudgetwithExpenses(budget, sum);
 });
